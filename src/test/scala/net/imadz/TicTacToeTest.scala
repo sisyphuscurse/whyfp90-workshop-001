40c8505edcbd3f6e2e836d2bcb86fb3d93cd8e54
Barry Zhong <zhongdj@gmail.com>
2022-03-13 00:46:20 +0800

    增加测试用例

--- src/test/scala/net/imadz/TicTacToeTest.scala
+++ src/test/scala/net/imadz/TicTacToeTest.scala
@@ -1,10 +1,41 @@
 package net.imadz
 
+import net.imadz.TicTacToeGame.ChessBoard.allWinningSolutions
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class TicTacToeTest extends AnyFlatSpec with should.Matchers {
 
+  "The algorithm" should "generate all the winning solutions in 2" in {
+    // given
+    val rows, columns, win = 2
+    // when
+    val solutions = allWinningSolutions(rows, columns, win)
+    solutions.should(contain(Set((0, 0), (0, 1))))
+    solutions.should(contain(Set((0, 0), (1, 0))))
+    solutions.should(contain(Set((0, 0), (1, 1))))
+    solutions.should(contain(Set((0, 1), (1, 1))))
+    solutions.should(contain(Set((0, 1), (1, 0))))
+    solutions.should(contain(Set((1, 0), (1, 1))))
+    solutions.foreach(println)
+    solutions.size shouldBe (6)
+  }
 
+  "The algorithm" should "generate all the winning solutions in tic-tac-toe" in {
+    // given
+    val rows, columns, win = 3
+    // when
+    val solutions = allWinningSolutions(rows, columns, win)
+    solutions.should(contain(Set((0, 0), (0, 1), (0, 2))))
+    solutions.should(contain(Set((0, 0), (1, 0), (2, 0))))
+    solutions.should(contain(Set((0, 0), (1, 1), (2, 2))))
+    solutions.should(contain(Set((0, 1), (1, 1), (2, 1))))
+    solutions.should(contain(Set((0, 2), (1, 1), (2, 0))))
+    solutions.should(contain(Set((0, 2), (1, 2), (2, 2))))
+    solutions.should(contain(Set((1, 0), (1, 1), (1, 2))))
+    solutions.should(contain(Set((2, 0), (2, 1), (2, 2))))
+    solutions.foreach(println)
+    solutions.size shouldBe (8)
+  }
 
 }
